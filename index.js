27f49d8aa8589fb1d8c799fc18dfa3f02624613c
CheeseCake <grabarzpolski@gmail.com>
2024-03-02 22:47:49 +0100

    about and gallery added

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 $(function () {
-    $("#navbar").load("./pages/common/navbar/navbar.html");
-    $("#jumbotron").load("./pages/common/jumbotron/jumbotron.html");
-    $("#footer").load("./pages/common/footer/footer.html");
+    var navbar = $("#navbar").load("./pages/common/navbar/navbar.html");
+    var jumbotron = $("#jumbotron").load("./pages/common/jumbotron/jumbotron.html");
+    var carausel = $("#carousel").load("./pages/common/carousel/carousel.html");
+    var footer = $("#footer").load("./pages/common/footer/footer.html");
+    var gallery = $("#gallery").load("./pages/common/gallery/gallery.html");
+    var about = $("#about").load("./pages/common/about/about.html");
+
+
+    about.addClass("hidden");
 });
--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 $(() => {
-    $("#navbar").load("./pages/common/navbar/navbar.html");
-    $("#jumbotron").load("./pages/common/navbar/navbar.html");
-    $("#footer").load("./pages/common/footer/footer.html");
+    
+    const navbar = $("#navbar").load("./pages/common/navbar/navbar.html");
+    const jumbotron = $("#jumbotron").load("./pages/common/jumbotron/jumbotron.html");
+    const carausel = $("#carousel").load("./pages/common/carousel/carousel.html");
+    const footer = $("#footer").load("./pages/common/footer/footer.html");
+    const about = $("#about").load("./pages/common/about/about.html");
+
+    about.addClass("hidden")
+    
 });
